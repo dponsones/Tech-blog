fb718df790bde9778f50f64ae934280c06f9a26d
Deion Ponsones <dcponson@gmail.com>
2023-05-29 22:31:38 -0400

    heroku push

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
-
+// for push
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
